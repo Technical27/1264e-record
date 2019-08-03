2202a16a9a5b29af03809ea1edae5235cf0bd877
Aamaruvi Yogamani <38222826+Technical27@users.noreply.github.com>
2019-08-03 12:13:41 -0400

    changed motor ports

--- src/init.cpp
+++ src/init.cpp
@@ -2,10 +2,10 @@
 
 std::vector<std::unique_ptr<double[]>> loadedAuton;
 
-Motor frontLeft (4, false, AbstractMotor::gearset::green);
-Motor frontRight (14, true, AbstractMotor::gearset::green);
-Motor backLeft (15, false, AbstractMotor::gearset::green);
-Motor backRight (5, true, AbstractMotor::gearset::green);
+Motor frontLeft (10, false, AbstractMotor::gearset::green);
+Motor frontRight (20, true, AbstractMotor::gearset::green);
+Motor backLeft (1, false, AbstractMotor::gearset::green);
+Motor backRight (11, true, AbstractMotor::gearset::green);
 
 MotorGroup left ({frontLeft, backLeft});
 MotorGroup right ({frontRight, backRight});
