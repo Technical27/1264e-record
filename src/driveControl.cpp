24886b70649e1c765439bcfcb7e582d29e410721
Aamaruvi Yogamani <38222826+Technical27@users.noreply.github.com>
2019-08-08 19:20:43 -0400

    fixed recorder

--- src/driveControl.cpp
+++ src/driveControl.cpp
@@ -3,32 +3,34 @@
 std::atomic_bool rec (true);
 
 void record (void*) {
-	int start = pros::millis();
 	std::unique_ptr<double[]> doubles;
 	while (rec) {
 		doubles = std::make_unique<double[]>(4);
 
-		doubles[1] = frontRight.getActualVelocity();
-		doubles[2] = frontLeft.getActualVelocity();
-		doubles[3] = backRight.getActualVelocity();
-		doubles[4] = backLeft.getActualVelocity();
+		doubles[0] = frontRight.getActualVelocity();
+		doubles[1] = frontLeft.getActualVelocity();
+		doubles[2] = backRight.getActualVelocity();
+		doubles[3] = backLeft.getActualVelocity();
 
 		loadedAuton.push_back(std::move(doubles));
 
-		pros::Task::delay(250);
-
-		if (start + 15000 < pros::millis()) rec = false;
+		pros::Task::delay(20);
 	}
+	saveAuton("/usd/auton");
+	controller.rumble(".");
 }
 
 void opcontrol() {
 	while (!controller.getDigital(ControllerDigital::A)) pros::Task::delay(10);
 	controller.rumble(".");
 	pros::Task recordTask (record);
-	while (rec) {
+	while (true) {
+		if (controller.getDigital(ControllerDigital::B)) break;
 		left.moveVoltage(controller.getAnalog(ControllerAnalog::leftY) * 12000);
   	right.moveVoltage(controller.getAnalog(ControllerAnalog::rightY) * 12000);
 	}
-	saveAuton("/usd/auton");
-	controller.rumble(".");
+	rec = false;
+	left.moveVoltage(0);
+	right.moveVoltage(0);
+	while (true) pros::Task::delay(10);
 }
